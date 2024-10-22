d0a2ebd5505a3b36c8250f3bdacd0e3926361de7
AnaCaroline04 <134461085+AnaCaroline04@users.noreply.github.com>
2024-10-22 11:37:07 -0300

    Add files via upload

---
+++ redesFavoritasMundo.js
@@ -0,0 +1,11 @@
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = []
+} 
+
+redesFavoritasMundo()
